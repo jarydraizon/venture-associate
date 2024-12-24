d2bf187d337ed19f5c3ffa8b30c9c8a2dfaeb1d4
Jaryd Raizon <jraizon@gmail.com>
2024-12-24 15:27:14 +0000

    Automatic commit before Assistant edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92e97e4d03b032b4"; }
+/******/ 		__webpack_require__.h = function() { return "fae4acd136ea14d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   deps = [
+    pkgs.psmisc
     pkgs.rustc
     pkgs.libiconv
     pkgs.cargo
