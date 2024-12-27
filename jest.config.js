cd247da3e5acb861c425a220ab24dfaf33df3a75
Jaryd Raizon <jraizon@gmail.com>
2024-12-27 12:59:54 +0000

    Automatic commit before Assistant edits

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,7 @@
 
 module.exports = {
   testEnvironment: 'jsdom',
+  setupFiles: ['dotenv/config'], // Add this line
   testEnvironmentOptions: {
     url: 'http://localhost' // Keep this as localhost since you're running locally
   },
