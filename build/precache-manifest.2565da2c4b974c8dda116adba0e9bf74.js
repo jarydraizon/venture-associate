b04addffc7f7f51787f8f7f86d32c40590dade6f
jarydraizon <36423289-jarydraizon@users.noreply.replit.com>
2025-02-26 01:01:47 +0000

    User checkpoint: Update build files to reflect changes in main.js and associated files.  Addresses styling issues on the /ventures/{venturename} page.

--- build/precache-manifest.2565da2c4b974c8dda116adba0e9bf74.js
+++ build/precache-manifest.2565da2c4b974c8dda116adba0e9bf74.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50e3071a76a21b52e8a1b2c67cebe8e8",
+    "revision": "a824fea82ce72ea0641a5cdc1b654a88",
     "url": "/index.html"
   },
   {
-    "revision": "2fc90b981d6d217a1f0d",
+    "revision": "4ca89aadf4fdb21cc59e",
     "url": "/static/css/main.42fef1cf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6988e2a8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2fc90b981d6d217a1f0d",
-    "url": "/static/js/main.9b2f7b51.chunk.js"
+    "revision": "4ca89aadf4fdb21cc59e",
+    "url": "/static/js/main.a40ebf10.chunk.js"
   },
   {
     "revision": "4ec709b70b2af75da58c",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8467c6d06055d84a2d858ba6f1ecab89.js"
+  "/precache-manifest.2565da2c4b974c8dda116adba0e9bf74.js"
 );
 
 self.addEventListener('message', (event) => {
