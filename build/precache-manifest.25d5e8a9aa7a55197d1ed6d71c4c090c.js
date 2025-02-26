ebad695988c4652b76db869a2dd5d4f39f3feef6
jarydraizon <36423289-jarydraizon@users.noreply.replit.com>
2025-02-26 00:35:34 +0000

    User checkpoint: Fix: Resolved issue where VenturePage displayed nothing; rebuilt application.

---
+++ build/precache-manifest.25d5e8a9aa7a55197d1ed6d71c4c090c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6732c5f6651b10d690f2d8fc96b7703b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94abe5d13ecabc28947",
+    "url": "/static/css/main.b642f162.chunk.css"
+  },
+  {
+    "revision": "86b20f4c6deb7f0d8418",
+    "url": "/static/js/2.6988e2a8.chunk.js"
+  },
+  {
+    "revision": "c82ef7b7cc3117f2586e80394dcc1e05",
+    "url": "/static/js/2.6988e2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94abe5d13ecabc28947",
+    "url": "/static/js/main.4d266916.chunk.js"
+  },
+  {
+    "revision": "4ec709b70b2af75da58c",
+    "url": "/static/js/runtime-main.fe34e980.js"
+  }
+]);
